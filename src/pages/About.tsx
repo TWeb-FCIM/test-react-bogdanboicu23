2a401f1fbdfe1b1b7da01da5f87c570665ee5898
Bogdan Boicu <bogdan@codbun.com>
2026-02-25 18:42:54 +0200

    another fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import '../styles/pages.css'
 
 export default function About() {
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import '../styles/pages.css'
 
 export default function Contact() {
